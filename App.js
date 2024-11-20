d318280dd2cbbebdedf05ce4f18744e33219c6a4
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-11-20 10:51:55 +0100

    getting started with navigation

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='light'/>
-   <CategoriesScreen />
-   </>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="MealCategories" 
+            component={CategoriesScreen} 
+            options={{ title: "Meal Categories" }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
