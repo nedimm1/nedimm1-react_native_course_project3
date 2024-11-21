67d1b028c17f0984b34a64600a45a7aabc7b1e68
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-11-21 14:39:59 +0100

    displaying images

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,14 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, Image, StyleSheet } from 'react-native';
 
-function MealItem({ title }) {
+function MealItem({ title, imageUrl }) {
   return (
-    <View style={styles.container}>
-      <Text>{title}</Text>
+    <View>
+      <Pressable>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -11,15 +16,14 @@ function MealItem({ title }) {
 export default MealItem;
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    marginVertical: 8,
-    backgroundColor: '#f5f5f5',
-    borderRadius: 8,
-    elevation: 4, // For Android shadow
-    shadowColor: 'black',
-    shadowOpacity: 0.25,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8, // For iOS shadow
+  image: {
+    width: "100%",
+    height: 200
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18
   },
 });
+
