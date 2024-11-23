2b47bf1bbd39bc636003bccc28c78abe8f28d1d0
Nedim Muminovic <nedimmuminovic61@gmail.com>
2024-11-23 16:59:24 +0100

    dynamic option navigation

--- screens/ MealsOverviewScreen.js
+++ screens/ MealsOverviewScreen.js
@@ -1,13 +1,26 @@
+import { useLayoutEffect } from "react";
+import { CATEGORIES } from "../data/dummy-data";
 import { MEALS } from "../data/dummy-data";
 import { StyleSheet, View, FlatList } from "react-native";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+  
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+  
 
   function renderMealItem(itemData) {
     const item = itemData.item;
